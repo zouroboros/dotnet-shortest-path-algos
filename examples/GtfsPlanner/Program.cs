89188151f3c37e49120cfe57c997e882f7df653b
Zouroboros <zouroboros@zoho.eu>
2025-06-19 17:24:34 +0200

    Removed unneeded methods and classes.

--- examples/GtfsPlanner/Program.cs
+++ examples/GtfsPlanner/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System.CommandLine;
 using System.Diagnostics;
-using System.Globalization;
 using Algorithm;
 using Graph;
-using Graph.Mutable;
 using GtfsPlanner;
-using Graphs = GtfsPlanner.Graphs;
 
 var rootCommand = new RootCommand("GtsPlanner");
 var gtfsOption = new Option<FileInfo>("--gtfs", "Path to the GTFS file.");
@@ -30,10 +27,10 @@ var latestDepartureAndEarliestArrival = new Command("LatestDepartureEarliestArri
 
 rootCommand.Add(latestDepartureAndEarliestArrival);
 
-var earliestArrivalInExtendedGraph = new Command("EarliestArrivalInExtendedGraph");
-earliestArrivalInExtendedGraph.AddOption(departureTimeOption);
+var earliestArrivalInLineGraph = new Command("EarliestArrivalInLineGraph");
+earliestArrivalInLineGraph.AddOption(departureTimeOption);
 
-rootCommand.Add(earliestArrivalInExtendedGraph);
+rootCommand.Add(earliestArrivalInLineGraph);
 
 var gtfsImporter = new GtfsImporter();
 var display = new Display();
@@ -75,25 +72,10 @@ latestDepartureAndEarliestArrival.SetHandler((gtfsFile, date, start, destination
     
 }, gtfsOption, dateOption, startOption, endOption);
 
-earliestArrivalInExtendedGraph.SetHandler((gtfsFile, date, start, destination, startTime) =>
+earliestArrivalInLineGraph.SetHandler((gtfsFile, date, start, destination, startTime) =>
 {
     var temporalGraph = gtfsImporter.ImportGraphFromGtfs(gtfsFile, date);
-    var lineGraph = temporalGraph.ToLineGraph((incomingEdge, node, outgoingEdge) => node.Value.Name.Contains("Haupt") || outgoingEdge.Value.DepartureTime - incomingEdge.Value.ArrivalTime < TimeSpan.FromMinutes(30));
-
-    Console.WriteLine($"Total number of nodes in line graph {lineGraph.Nodes.Count} and edges {lineGraph.Edges.Count()}");
-    
-    var nodesWithOnlyOneEdge = lineGraph.Nodes.Where(node => node.Edges.Count(edge => edge.NodeA == node) <= 1);
-    
-    Console.WriteLine($"Number with at most one outgoing edge in line graph {nodesWithOnlyOneEdge.Count()}" );
-
-    var mutableGraph = MutableGraph.From(lineGraph,
-        new Func<(string Id, string Name), ICollection<(string Id, string Name)>>(value => [value]));
-    
-    Console.WriteLine($"Original graph: #Nodes {mutableGraph.Nodes.Count} #Edges {mutableGraph.Edges.Count()}");
-    
-    mutableGraph.Simplify();
-    
-    Console.WriteLine($"Simplified graph: #Nodes {mutableGraph.Nodes.Count} #Edges {mutableGraph.Edges.Count()}");
+    var lineGraph = temporalGraph.ToLineGraph();
     
     var startNode = temporalGraph.Nodes.First(node => node.Value.Name == start).Value;
     var destinationNode = temporalGraph.Nodes.First(node => node.Value.Name == destination).Value;
@@ -102,11 +84,11 @@ earliestArrivalInExtendedGraph.SetHandler((gtfsFile, date, start, destination, s
     stopwatch.Start();
     var paths = PathFinder
         .FindEarliestArrivalPaths(lineGraph, startNode, destinationNode, new DateTime(date, startTime))
-        .Select(edges => edges.ToArray()).ToArray();
+        .Select(edges => edges.ToArray()).First();
     stopwatch.Stop();
 
     display.DisplayCalculationTimeAndNumberOfResults(stopwatch.Elapsed, paths.Length);
-    display.DisplayPaths(paths, label => label.Name);
+    display.DisplayPath(paths, label => label.Name);
     
     
 }, gtfsOption, dateOption, startOption, endOption, departureTimeOption);
--- src/Graph/Mutable/MutableEdge.cs
+++
@@ -1,8 +0,0 @@
-namespace Graph.Mutable;
-
-public record MutableEdge<TNode, TEdge>(MutableNode<TNode, TEdge> NodeA, MutableNode<TNode, TEdge> NodeB) : IEdge<TNode, TEdge>
-{
-    public required TEdge Value { get; set; }
-    INode<TNode, TEdge> IEdge<TNode, TEdge>.NodeA => NodeA;
-    INode<TNode, TEdge> IEdge<TNode, TEdge>.NodeB => NodeB;
-}
